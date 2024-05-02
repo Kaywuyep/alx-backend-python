83325f5e8c97e5348ab01a4a08915ffd45af2ff6
Kaywuyep <wuyepkate@gmail.com>
2024-05-02 15:07:09 +0000

    task

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""a sum list module"""
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """a sum list module"""
+    return sum(input_list)
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+""" a simple module"""
+
+
+def sum_mixed_list() ->:
