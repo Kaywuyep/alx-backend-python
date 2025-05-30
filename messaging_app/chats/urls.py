721e2c644d52a4233c4821cdbf7bba1e9f71df85
catherinewuyep-at-901044882194 <AIk1wnTTmD4HJphZCOBvtgdYTEeDf+ujxTxnBMUFaM1n0PbUQ1yT0BNfnNc=>
2025-05-30 18:59:18 +0100

    chats messaging app chats/urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,5 +1,7 @@
+# pylint: disable=no-member
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_nested import routers as nested_routers
 from .views import ConversationViewSet, MessageViewSet
 
 # Create a router and register our viewsets with it
@@ -7,7 +9,14 @@ router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet)
 router.register(r'messages', MessageViewSet)
 
+# Create nested router for messages within conversations
+conversations_router = nested_routers.NestedDefaultRouter(
+    router, r'conversations', lookup='conversation')
+conversations_router.register(
+    r'messages', MessageViewSet, basename='conversation-messages')
+
 # The API URLs are now determined automatically by the router
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(conversations_router.urls)),
 ]
