a03da8883dba8d1a2acafc15cdd9cd7ffff4c5bd
catherinewuyep-at-901044882194 <AIk1wnTTmD4HJphZCOBvtgdYTEeDf+ujxTxnBMUFaM1n0PbUQ1yT0BNfnNc=>
2025-06-08 18:17:40 +0100

    Middleware

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     # custom middleware
     'chats.middleware.RequestLoggingMiddleware',
     'chats.middleware.RestrictAccessByTimeMiddleware',
-    'chats.middleware.RolePermissionMiddleware',
+    'chats.middleware.RolepermissionMiddleware',
     'chats.middleware.OffensiveLanguageMiddleware',
 ]
 
